fd124da658242367a380f539fa7f9b672b1d2f58
harshalchavan7 <harshalschavan2003@gmail.com>
2024-05-11 14:11:34 +0530

    Updated binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 bool binarySearch(vector<int> arr, int key)
 {
-
-    int low=0, high=arr.size()-1;
+    vector<int> sortedArr=arr;
+    sort(sortedArr.begin(),sortedArr.end());
+    int low=0, high=sortedArr.size()-1;
     while(low<=high)
     {
         int mid=low+(high-low)/2;
-        if(arr[mid]<key) low=mid+1;
-        else if(arr[mid]>key) high=mid-1;
+        if(sortedArr[mid]<key) low=mid+1;
+        else if(sortedArr[mid]>key) high=mid-1;
         else return true;
     }
     return false;
@@ -18,8 +20,8 @@ bool binarySearch(vector<int> arr, int key)
 
 int main()
 {
-    vector<int> arr={1,2,3,4,5,6,7,8,9};
-    if(binarySearch(arr,3)) cout<<"Found!";
-    else cout<<"Not found";
+    vector<int> arr={71,65,31,4354,556,6234,756,8089,924};
+    if(binarySearch(arr,381)) cout<<"Found!";
+    else cout<<"Not found.";
     return 0;
 }
